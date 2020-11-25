780771a7222f46b471689947b39ea8fff41761ce
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-11-25 14:12:57 +0200

    added JavaDoc

--- core/src/main/java/greencity/security/filters/Storage.java
+++ core/src/main/java/greencity/security/filters/Storage.java
@@ -6,10 +6,16 @@ import java.util.Map;
 public class Storage {
     private static final Map<String, String> storage = new HashMap<>();
 
+    /**
+     * Put value to map.
+     * */
     public static void put(String key, String value) {
         storage.put(key, value);
     }
 
+    /**
+     * Get value from map.
+     * */
     public static String get(String key) {
         return storage.get(key);
     }
--- core/src/main/java/greencity/security/request/MutableHttpServletRequest.java
+++ core/src/main/java/greencity/security/request/MutableHttpServletRequest.java
@@ -8,15 +8,24 @@ import java.util.Map;
 public class MutableHttpServletRequest extends HttpServletRequestWrapper {
     private final Map<String, String> headers;
 
+    /**
+     * Constructor.
+     * */
     public MutableHttpServletRequest(HttpServletRequest request) {
         super(request);
         this.headers = new HashMap<>();
     }
 
+    /**
+     * Put header to map.
+     * */
     public void putHeader(String name, String value) {
         this.headers.put(name, value);
     }
 
+    /**
+     * Get header from map.
+     * */
     public String getHeader(String name) {
         String headerValue = headers.get(name);
         if (headerValue != null) {
